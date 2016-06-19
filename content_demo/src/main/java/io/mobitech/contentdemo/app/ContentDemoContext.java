46bff1e19045dae8a7b9d6ecd22e251c6eb769bb
Gal Levinsky <gal.levinsky@gmail.com>
2016-06-19 15:16:25 +0300

    inline documentation

--- content_demo/src/main/java/io/mobitech/contentdemo/app/ContentDemoContext.java
+++ content_demo/src/main/java/io/mobitech/contentdemo/app/ContentDemoContext.java
@@ -32,29 +32,36 @@ public class ContentDemoContext extends Application {
         super.onCreate();
 
 
-        //callback for user id
+        //callback for user id:
+        //Either takes the user's advertiserId or creates a unique ID, if
         AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
             @Override
             protected String doInBackground(Void... params) {
                 try {
                     AdvertisingIdClient.Info adInfo =  AdvertisingIdClient.getAdvertisingIdInfo(ContentDemoContext.this);
-                    if (!adInfo.isLimitAdTrackingEnabled()){
+                    if (!adInfo.isLimitAdTrackingEnabled()) {//if user hasn't opt-out
                         return  adInfo.getId();
                     }else{
-                        //generate
+                        //Settings.Secure.ANDROID_ID: A 64-bit number (as a hex string) that is randomly
+                        // generated when the user first sets up the device and should remain
+                        // constant for the lifetime of the user's device.
                         String android_id = Settings.Secure.getString(ContentDemoContext.this.getContentResolver(),
                                 Settings.Secure.ANDROID_ID);
+
+                        //if ANDROID_ID is unavailable - generate a random ID
                         return android_id == null ?  new UUID(System.currentTimeMillis(), System.currentTimeMillis()*2).toString() : android_id;
                     }
                 } catch (Exception e) {
                     Log.e(TAG, e.getMessage(), e);
                 }
+                //default return is empty, in order to catch issues with user id generation
                 return "";
             }
 
             @Override
             protected void onPostExecute(String advertId) {
                 userID = advertId;
+                //init Mobitech's content SDK
                 ContentService.init(ContentDemoContext.this, userID);
             }
 
